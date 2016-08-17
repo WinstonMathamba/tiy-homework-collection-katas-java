62e2636fccfc8449393c4b19c96579181f23f286
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-17 09:48:39 -0400

    pushing progress on Hashmaps

--- src/Kata.java
+++ src/Kata.java
@@ -2,47 +2,109 @@
 /**
  * Created by win808mac on 8/14/16.
  */
-import com.sun.deploy.util.ArrayUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 
 public class Kata {
 
-//First Last Six - Given an ArrayList of ints, return true if 6
-//appears as either the first or last element in the ArrayList.
-//The ArrayList will be length 1 or more.
+/**First Last Six - Given an ArrayList of ints, return true if 6
+ *appears as either the first or last element in the ArrayList.
+ *The ArrayList will be length 1 or more.*/
 
-    public boolean firstLastSix(ArrayList<Integer> nums) {
-      if (nums.get(0).equals(6) || (nums.get(nums.size() - 1).equals(6))) {
-          return true;
-      }
-      else {
-          return false;
-      }
-
-    }
-//Same First Last - Given an ArrayList of Integers, return true if the ArrayList
-//is length 1 or more, and the first element and the last element are equal.
-
-    public boolean sameFirstLast(ArrayList<Integer> num) {
-        return (num.size() >= 1 && num.get(0) == num.get(num.size()-1));
+    public static boolean firstLastSix(ArrayList<Integer> nums) {
+        if (nums.get(0).equals(6) || (nums.get(nums.size() - 1).equals(6))) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
-//Rotate Left 3 - Given an ArrayList of Integers length 3,
-//return an ArrayList with the elements "rotated left"
-// so [1, 2, 3] yields [2, 3, 1].
+/**Same First Last - Given an ArrayList of Integers, return true if the ArrayList
+ *is length 1 or more, and the first element and the last element are equal.*/
 
-    public void rotateLeft(ArrayList<Integer> num) {
+    public static boolean sameFirstLast(ArrayList<Integer> num) {
+        return (num.size() >= 1 && num.get(0) == num.get(num.size() - 1));
+
+    }
+
+/**Rotate Left 3 - Given an ArrayList of Integers length 3,
+ *return an ArrayList with the elements "rotated left"
+ *so [1, 2, 3] yields [2, 3, 1].*/
+
+    public static ArrayList<Integer> rotateLeft(ArrayList<Integer> num) {
         Collections.rotate(num, num.size() - 2);
+        return num;
 
     }
 
-//Reverse 3 - Given an ArrayList of Integers length 3, return a new ArrayList
-//with the elements in reverse order, so [1, 2, 3] becomes [3, 2, 1].
+ /**Reverse 3 - Given an ArrayList of Integers length 3, return a new ArrayList
+  *with the elements in reverse order, so [1, 2, 3] becomes [3, 2, 1].*/
 
-    public void reverse(ArrayList<Integer> num) {
-        ArrayUtil.
+    public static ArrayList<Integer> reverse3(ArrayList<Integer> num) {
+        if (num.size() > 1) {
+            Integer value = num.remove(0);
+            reverse3(num);
+            num.add(value);
+        }
+        return num;
+    }
+
+/**Sum First Penultimate - Given an ArrayList of Integers of at least length 2,
+ *return the sum of the first elements in the ArrayList and the second from last element.
+ *If the ArrayList length is less than 2, just sum up the elements that exist,
+ *returning 0 if the array is length 0.*/
+
+    public static int sumFirstPenultimate(ArrayList<Integer> num) {
+        if (num.size() == 0) {
+            return 0;
+        } else if (num.size() == 1) {
+            return num.get(0);
+        } else {
+            return num.get(0) + num.get(num.size() - 2);
+        }
+    }
+
+/**Map Bully
+ *Modify and return the given HashMap as follows: if the key "a" has a value,
+ *set the key "b" to have that value, and set the key "a" to have the value "".
+ *Basically "b" is a bully, taking the value of "a".    */
+
+    public static HashMap<String, String> mapBully(HashMap<String, String> data) {
+        if (data.containsKey("a")) {
+            data.put("b", data.get("a"));
+            data.put("a", "");
+        }
+        return data;
+
+    }
+
+/**Map Share - Modify and return the given HashMap as follows:
+*if the key "a" has a value, set the key "b" to have that same value.
+*In all cases remove the key "c", leaving the rest of the map unchanged.*/
+
+    public static HashMap<String, String> mapShare(HashMap<String, String> data) {
+        if (data.containsKey("a")) {
+            data.put("b", data.get("a"));
+        }
+        data.remove("c");
+        return data;
+    }
+
+/** Map AB - Modify and return the given HashMap as follows: for this problem the
+ *HashMap may or may not contain the "a" and "b" keys. If both keys are present,
+ *append their 2 string values together and store the result under the key "ab" */
+
+    public static HashMap<String, String> mapAB(HashMap<String, String> data) {
+        if(data.containsKey("a") && data.containsKey("b")) {
+            HashMap<String> absmoosh = data.get("a"), data.get("b");
+            data.put("ab",);
+
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,117 +3,254 @@
  */
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-
 
 public class Main {
     public static void main(String[] args) {
-        Kata kata = new Kata();
 
         ArrayList<Integer> ints = new ArrayList<>();
+        HashMap<String,String> map = new HashMap<>();
+
 
         ints.add(1);
         ints.add(2);
         ints.add(6);
 
-        boolean result = kata.firstLastSix(ints);
+        boolean result = Kata.firstLastSix(ints);
         System.out.printf("Kata.firstLastSix(%s) -> %s\n",ints, result);
 
         ints.add(0, 6);
         ints.add(3,3);
         ints.remove(4);
-        result = kata.firstLastSix(ints);
         System.out.printf("Kata.firstLastSix(%s) -> %s\n",ints, result);
 
         ints.add(0,13);
-        result = kata.firstLastSix(ints);
         System.out.printf("Kata.firstLastSix(%s) -> %s\n",ints, result);
 
         ints.add(5,6);
-        result = kata.firstLastSix(ints);
         System.out.printf("Kata.firstLastSix(%s) -> %s\n",ints, result);
 
 
         ints.remove(0);
         ints.remove(1);
         ints.remove(ints.size()-1);
-        result = kata.firstLastSix(ints);
         System.out.printf("Kata.firstLastSix(%s) -> %s\n",ints, result);
 
         ints.remove(0);
-        result = kata.firstLastSix(ints);
         System.out.printf("Kata.firstLastSix(%s) -> %s\n",ints, result);
         System.out.println();
 
-        //example of Same First Last
+//example of Same First Last
 
         ints.add(0,1);
-        result = kata.sameFirstLast(ints);
+        result = Kata.sameFirstLast(ints);
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n",ints, result);
 
         ints.add(1);
-        result = kata.sameFirstLast(ints);
+        result = Kata.sameFirstLast(ints);
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n",ints, result);
 
         ints.remove(2);
-        result = kata.sameFirstLast(ints);
+        result = Kata.sameFirstLast(ints);
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n",ints, result);
 
         ints.add(0, 4);
-        result = kata.sameFirstLast(ints);
+        result = Kata.sameFirstLast(ints);
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n",ints, result);
 
         ints.remove(3);
-        result = kata.sameFirstLast(ints);
+        result = Kata.sameFirstLast(ints);
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n",ints, result);
 
         ints.add(4);
-        result = kata.sameFirstLast(ints);
+        result = Kata.sameFirstLast(ints);
         System.out.printf("Kata.sameFirstLast(%s) -> %s\n",ints, result);
         System.out.println();
 
-        //Example of Rotate Left
+//Example of Rotate Left
         ints.clear();
         ints.addAll(Arrays.asList(1,2,3));
         System.out.printf("Katas.rotateLeft(%s)", ints);
-        kata.rotateLeft(ints);
+        Kata.rotateLeft(ints);
         System.out.printf(" -> %s\n", ints);
 
         ints.clear();
         ints.addAll(Arrays.asList(5,11,9));
         System.out.printf("Katas.rotateLeft(%s)", ints);
-        kata.rotateLeft(ints);
+        Kata.rotateLeft(ints);
         System.out.printf(" -> %s\n", ints);
 
         ints.clear();
         ints.addAll(Arrays.asList(7,0,0));
         System.out.printf("Katas.rotateLeft(%s)", ints);
-        kata.rotateLeft(ints);
+        Kata.rotateLeft(ints);
         System.out.printf(" -> %s\n", ints);
 
         ints.clear();
         ints.addAll(Arrays.asList(4,2,0));
         System.out.printf("Katas.rotateLeft(%s)", ints);
-        kata.rotateLeft(ints);
+        Kata.rotateLeft(ints);
         System.out.printf(" -> %s\n", ints);
 
         ints.clear();
         ints.addAll(Arrays.asList(6,7,8));
         System.out.printf("Katas.rotateLeft(%s)", ints);
-        kata.rotateLeft(ints);
+        Kata.rotateLeft(ints);
         System.out.printf(" -> %s\n", ints);
 
         ints.clear();
         ints.addAll(Arrays.asList(21,22,23));
         System.out.printf("Katas.rotateLeft(%s)", ints);
-        kata.rotateLeft(ints);
+        Kata.rotateLeft(ints);
         System.out.printf(" -> %s\n", ints);
         System.out.println();
 
-        //Examples of Reverse 3
-        System.out.printf("Katas.reverse(%s)", ints);
-        kata.reverse(ints);
+//Examples of Reverse 3
+        ints.clear();
+        ints.addAll(Arrays.asList(1,2,3));
+        System.out.printf("Katas.reverse3(%s)", ints);
+        Kata.reverse3(ints);
         System.out.printf(" --> %s\n", ints);
+
+        ints.clear();
+        ints.addAll(Arrays.asList(5,11,9));
+        System.out.printf("Katas.reverse3(%s)", ints);
+        Kata.reverse3(ints);
+        System.out.printf(" --> %s\n", ints);
+
+        ints.clear();
+        ints.addAll(Arrays.asList(7,0,0));
+        System.out.printf("Katas.reverse3(%s)", ints);
+        Kata.reverse3(ints);
+        System.out.printf(" --> %s\n", ints);
+
+        ints.clear();
+        ints.addAll(Arrays.asList(4,8,12));
+        System.out.printf("Katas.reverse3(%s)", ints);
+        Kata.reverse3(ints);
+        System.out.printf(" --> %s\n", ints);
+
+        ints.clear();
+        ints.addAll(Arrays.asList(8,16,24));
+        System.out.printf("Katas.reverse3(%s)", ints);
+        Kata.reverse3(ints);
+        System.out.printf(" --> %s\n", ints);
+        System.out.println();
+
+//Example of Sum First Penultimate
+        ints.clear();
+        ints.addAll(Arrays.asList(1,2,3));
+        System.out.printf("Katas.sumFirstPenultimate%s", ints);
+        System.out.printf(" --> %s\n", Kata.sumFirstPenultimate(ints));
+
+        ints.clear();
+        ints.addAll(Arrays.asList(1,1));
+        System.out.printf("Katas.sumFirstPenultimate%s", ints);
+        System.out.printf(" --> %s\n", Kata.sumFirstPenultimate(ints));
+
+        ints.clear();
+        ints.addAll(Arrays.asList(1,1,1,1));
+        System.out.printf("Katas.sumFirstPenultimate%s", ints);
+        System.out.printf(" --> %s\n", Kata.sumFirstPenultimate(ints));
+
+        ints.clear();
+        ints.addAll(Arrays.asList(1,2,3,4));
+        System.out.printf("Katas.sumFirstPenultimate%s", ints);
+        System.out.printf(" --> %s\n", Kata.sumFirstPenultimate(ints));
+
+        ints.clear();
+        ints.addAll(Arrays.asList(5,1,3,4));
+        System.out.printf("Katas.sumFirstPenultimate%s", ints);
+        System.out.printf(" --> %s\n", Kata.sumFirstPenultimate(ints));
+
+        ints.clear();
+        ints.addAll(Arrays.asList(10,1,5,6));
+        System.out.printf("Katas.sumFirstPenultimate%s", ints);
+        System.out.printf(" --> %s\n", Kata.sumFirstPenultimate(ints));
+        System.out.println();
+
+//Example of mapBully
+
+        map.put("a","candy");
+        map.put("b","dirt");
+        System.out.printf("Kata for Map Bully %s",map);
+        System.out.printf(" --> %s\n ",Kata.mapBully(map));
+
+        map.clear();
+        map.put("a","candy");
+        map.put("b","");
+        System.out.printf("Kata for Map Bully %s",map);
+        System.out.printf(" --> %s\n ",Kata.mapBully(map));
+
+        map.clear();
+        map.put("b","carrot");
+        map.put("c","meh");
+        map.put("a","candy");
+        System.out.printf("Kata for Map Bully %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapBully(map));
+
+        map.clear();
+        map.put("a","lunch money");
+        map.put("b","crackers");
+        map.put("c","pb&j");
+        System.out.printf("Kata for Map Bully %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapBully(map));
+
+        map.clear();
+        map.put("a","homework");
+        map.put("b","lint");
+        System.out.printf("Kata for Map Bully %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapBully(map));
+
+        map.clear();
+        map.put("a","ball");
+        map.put("a","sneakers");
+        map.put("c","jacket");
+        System.out.printf("Kata for Map Bully %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapBully(map));
+        System.out.println();
+
+//Examples of Map Share -
+        map.clear();
+        map.put("a","aaa");
+        map.put("b","bbb");
+        map.put("c","ccc");
+        System.out.printf("Kata for Map Share %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapShare(map));
+
+        map.clear();
+        map.put("b","xyz");
+        map.put("c","ccc");
+        System.out.printf("Kata for Map Share %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapShare(map));
+
+        map.clear();
+        map.put("d","hi");
+        map.put("c","meh");
+        map.put("a","aaa");
+        System.out.printf("Kata for Map Share %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapShare(map));
+
+        map.clear();
+        map.put("b","lou");
+        map.put("c","dew");
+        map.put("a","poo");
+        System.out.printf("Kata for Map Share %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapShare(map));
+
+        map.clear();
+        map.put("a","choo");
+        map.put("c","seadoo");
+        map.put("a","threw");
+        System.out.printf("Kata for Map Share %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapShare(map));
+
+        map.clear();
+        map.put("e","cat");
+        map.put("a","dog");
+        map.put("c","fish");
+        System.out.printf("Kata for Map Share %s ", map);
+        System.out.printf(" --> %s\n", Kata.mapShare(map));
+
     }
 }
