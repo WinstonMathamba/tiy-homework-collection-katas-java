d8843219e038f6bc574ac6e3759c7b841b0b0c05
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-24 15:47:13 -0400

    final for doug

--- src/Kata.java
+++ src/Kata.java
@@ -41,8 +41,8 @@ public class Kata {
 
     }
 
- /**Reverse 3 - Given an ArrayList of Integers length 3, return a new ArrayList
-  *with the elements in reverse order, so [1, 2, 3] becomes [3, 2, 1].*/
+/**Reverse 3 - Given an ArrayList of Integers length 3, return a new ArrayList
+ *with the elements in reverse order, so [1, 2, 3] becomes [3, 2, 1].*/
 
     public static ArrayList<Integer> reverse3(ArrayList<Integer> num) {
         if (num.size() > 1) {
